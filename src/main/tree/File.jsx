c57d5d3aace3d1f0603f9d22f79429623e9bdff1
frozar <fabien.rozar@gmail.com>
2022-03-20 17:57:27 +0400

    Open fil on double click.

--- src/main/tree/File.jsx
+++ src/main/tree/File.jsx
@@ -2,7 +2,16 @@ const File = (props) => {
   const { node } = props;
   return (
     <li id={node.id} class="py-1">
-      <span class="selectable file" tabindex="0">
+      <span
+        class="selectable file"
+        tabindex="0"
+        onClick={(e) => {
+          // Handle only double click
+          if (e.detail === 2) {
+            window.open(node.webViewLink, "_blank").focus();
+          }
+        }}
+      >
         <img src={node.iconLink} />
         <span
           style="margin-left: 4px; margin-right: 2px"
